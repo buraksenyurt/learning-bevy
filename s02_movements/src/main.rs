cf6b6900d031d07a82ef8ca7fa551f54f7967a1f
buraksenyurt <burakselimsenyurt@gmail.com>
2023-01-05 15:46:39 +0300

    Integrated a movement system to the game.

--- s02_movements/src/main.rs
+++ s02_movements/src/main.rs
@@ -1,15 +1,22 @@
 use bevy::app::App;
 use bevy::prelude::*;
+use bevy::time::FixedTimestep;
 use bevy::window::close_on_esc;
 use bevy::DefaultPlugins;
 use std::f32::consts::PI;
 
 const BOUNDS: Vec2 = Vec2::new(1200.0, 640.0);
+const DELTA_TIME: f32 = 1. / 60.;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup_system)
+        .add_system_set(
+            SystemSet::new()
+                .with_run_criteria(FixedTimestep::step(DELTA_TIME as f64))
+                .with_system(fighter_movement_system),
+        )
         .add_system(close_on_esc)
         .run();
 }
@@ -37,4 +44,34 @@ fn setup_system(mut commands: Commands, asset_server: Res<AssetServer>) {
             rotation_speed: 2. * PI,
         },
     ));
+}
+
+fn fighter_movement_system(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<(&Fighter, &mut Transform)>,
+) {
+    let (fighter, mut transform) = query.single_mut();
+    let mut rotation_factor = 0.;
+    let mut movement_factor = 0.;
+
+    if keyboard_input.pressed(KeyCode::Left) {
+        rotation_factor += 0.5;
+    } else if keyboard_input.pressed(KeyCode::Right) {
+        rotation_factor -= 0.5;
+    }
+    if keyboard_input.pressed(KeyCode::Up) {
+        movement_factor += 0.75;
+    }
+    transform.rotate_z(rotation_factor * fighter.rotation_speed * DELTA_TIME);
+
+    // Vec3::Y [0,1,0] birim vektörüdür.
+    // Dönüş açısının birim vektör ile çarpımı sayesinde hareket yönünü buluruz
+    let movement_direction = transform.rotation * Vec3::Y;
+    // yöne göre büyüklüğü hesaplarız
+    let movement_distance = movement_factor * fighter.movement_speed * DELTA_TIME;
+    let translation_delta = movement_direction * movement_distance;
+    transform.translation += translation_delta;
+
+    let range = Vec3::from((BOUNDS / 2.0, 0.));
+    transform.translation = transform.translation.min(range).max(-range);
 }
