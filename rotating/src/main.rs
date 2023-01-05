6417503fd11d167155522e12157076780000beb7
buraksenyurt <burakselimsenyurt@gmail.com>
2023-01-05 14:11:42 +0300

    Added the movement system to player.

--- rotating/src/main.rs
+++ rotating/src/main.rs
@@ -1,15 +1,21 @@
+use bevy::math::Vec3Swizzles;
 use bevy::prelude::*;
 use bevy::time::FixedTimestep;
 use bevy::window::close_on_esc;
 use std::f32::consts::PI;
 
 const BOUNDS: Vec2 = Vec2::new(800., 600.);
+const FPS: f32 = 1. / 30.;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup_system)
-        .add_system_set(SystemSet::new().with_run_criteria(FixedTimestep::step(1. / 60.)))
+        .add_system_set(
+            SystemSet::new()
+                .with_run_criteria(FixedTimestep::step(FPS as f64))
+                .with_system(player_movement_system)
+        )
         .add_system(close_on_esc)
         .run();
 }
@@ -66,7 +72,7 @@ fn setup_system(mut commands: Commands, asset_server: Res<AssetServer>) {
             transform: Transform::from_xyz(0. - h_margin, 0., 0.),
             ..default()
         },
-        SnapToPlayer,
+        //SnapToPlayer,
     ));
     commands.spawn((
         SpriteBundle {
@@ -74,7 +80,7 @@ fn setup_system(mut commands: Commands, asset_server: Res<AssetServer>) {
             transform: Transform::from_xyz(0., 0. - v_margin, 0.),
             ..default()
         },
-        SnapToPlayer,
+        //SnapToPlayer,
     ));
 
     // RotateToPlayer'a verilen radyan değerleri ile,
@@ -96,4 +102,45 @@ fn setup_system(mut commands: Commands, asset_server: Res<AssetServer>) {
         },
         RotateToPlayer { vel: PI / 2. },
     ));
+}
+
+// Oyuncu için hareket sistemi
+// Resource ile klavye hareketlerini içeriye alıyoruz
+// query ile de Transform bileşeni barındıran oyuncu nesnesini sorgulayacağımızı belirtiyoruz
+fn player_movement_system(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<(&Player, &mut Transform)>,
+) {
+    // oyuncu nesnesini transform bileşeni ile birlikte yakaladık
+    let (p, mut transform) = query.single_mut();
+
+    let mut rot_factor = 0.;
+    let mut mov_factor = 0.;
+
+    // Sağ(D) ve sol(A) tuşlara basılması ile döndürme değerini bir birim artırıyoruz
+    if keyboard_input.pressed(KeyCode::A) {
+        rot_factor += 1.;
+    }
+    if keyboard_input.pressed(KeyCode::D) {
+        rot_factor -= 1.;
+    }
+    // Yukarı(W) tuşuna basıldığında ise hareket etme faktörünü bir birim artırıyoruz
+    if keyboard_input.pressed(KeyCode::W) {
+        mov_factor += 1.;
+    }
+    // z eksenine göre döndürme faktörü, oyuncunun hızı ve delta time değerinleri kullanarak
+    // oyuncu gemisine bir döndürme kazandırıyoruz
+    transform.rotate_z(rot_factor * p.vel * FPS);
+
+    //
+    let movement_direction = transform.rotation * Vec3::Y;
+    let movement_distance = mov_factor * p.vel * FPS;
+    let translation_delta = movement_direction * movement_distance;
+    transform.translation += translation_delta;
+
+    // oyuncu gemisinin oyun sahasının dışına çıkmasını engellemek için
+    // bir aralık tespiti gerekiyor.
+    let range = Vec3::from((BOUNDS / 2.0, 0.0));
+    // oyuncunun pozisyonu min ve max değerlerine göre sınırlandırılıyor.
+    transform.translation = transform.translation.min(range).max(-range);
 }
