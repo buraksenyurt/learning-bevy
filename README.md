b6b4845cd070e152675ba1147c562b0b4e68a790
buraksenyurt <burakselimsenyurt@gmail.com>
2023-01-06 21:29:08 +0300

    Added new shapes and integrated with scaling system.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Bevy oyun motorunu öğrenmek için açılmış alandır.
 
-- [ ] **s03_simple_shapes:** Çeşitli geometrik şekillerin nasıl çizdirildiğini öğrenmeye çalışıyorum.
+- [x] **s03_simple_shapes:** Çeşitli geometrik şekillerin nasıl çizdirildiğini öğrenmeye çalışıyorum. Kare, daire ve hexagonal formda üç şekil koyup yukarı,aşağı ok tuşlarına basıldığında ölçeklerini değiştiren bir sistemle birlikte.
 - [x] **s02_movements:** 01deki örnekten yola çıkarak sadece hareket sistemini tekrar ettiğim pratik.
 - [x] **s01_rotating:** Bevy resmi sitesindeki 2D Rotation isimli örneği uyguladım. Ekrana entity yerleştirme ve hareket, döndürme gibi matematiksel operasyonların uygulandığı sistemlerin nasıl yazıldığına baktım.
 - [x] **s00_hello_bevy:** Bevy resmi sitesindeki başlangıç öğretisi.
--- s03_simple_shapes/src/main.rs
+++ s03_simple_shapes/src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy::sprite::MaterialMesh2dBundle;
 
 fn main() {
     App::new()
@@ -9,17 +10,19 @@ fn main() {
 }
 
 #[derive(Component)]
-struct Square;
+struct Shape;
 
 fn setup_system(
     mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     commands.spawn(Camera2dBundle::default());
 
     commands.spawn((
         SpriteBundle {
             sprite: Sprite {
-                color: Color::rgba_u8(237, 43, 191, 100),
+                color: Color::rgba_u8(237, 43, 191, 250),
                 custom_size: Some(Vec2::new(64., 64.)),
                 ..default()
             },
@@ -29,13 +32,35 @@ fn setup_system(
             },
             ..default()
         },
-        Square,
+        Shape,
+    ));
+
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: meshes
+                .add(shape::RegularPolygon::new(32., 12).into())
+                .into(),
+            material: materials.add(ColorMaterial::from(Color::rgba_u8(43, 198, 238, 250))),
+            transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
+            ..default()
+        },
+        Shape,
+    ));
+
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: meshes.add(shape::Circle::new(32.).into()).into(),
+            material: materials.add(ColorMaterial::from(Color::rgba_u8(237, 146, 43, 250))),
+            transform: Transform::from_translation(Vec3::new(200., 0., 0.)),
+            ..default()
+        },
+        Shape,
     ));
 }
 
 fn square_scale_system(
     keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&Square, &mut Transform)>,
+    mut query: Query<(&Shape, &mut Transform)>,
 ) {
     for (_, mut tr) in query.iter_mut() {
         if keyboard_input.pressed(KeyCode::Up) {
